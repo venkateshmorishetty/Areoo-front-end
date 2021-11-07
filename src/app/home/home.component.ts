007b996bf6ca57e0a18cfbb0480a8e7b54f83671
venkateshmorishetty <venkateshmorishetty@gmail.com>
2021-11-07 23:21:19 +0530

    Added categories section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,9 @@ export class HomeComponent implements OnInit {
   categoriesData:any;
   slideConfig={"slidesToShow": 4, "slidesToScroll": 4}
   private marketPlace_jsonURL = './assets/marketplace.json';
-  private categories_jsonURL = './assets/categories.json'
+  private categories_jsonURL = './assets/categories.json';
+
+
  constructor(private http: HttpClient) {
   //getting market place json data 
   this.getMarketPlaceJSON().subscribe(data => {
@@ -23,6 +25,9 @@ export class HomeComponent implements OnInit {
    this.getCategoriesJSON().subscribe(data => {
     this.categoriesData = data;
    });
+
+   console.log("market place data "+this.marketPlaceData)
+   
  }
  public getMarketPlaceJSON(): Observable<any> {
   return this.http.get(this.marketPlace_jsonURL);
@@ -34,40 +39,8 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
+    
   }
 
-
-
-  slides = [
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"}
-  ];
-  // slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
-  
-  addSlide() {
-    this.slides.push({img: "http://placehold.it/350x150/777777"})
-  }
-  
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
-  
-  slickInit(e:any) {
-    console.log('slick initialized');
-  }
-  
-  breakpoint(e:any) {
-    console.log('breakpoint');
-  }
-  
-  afterChange(e:any) {
-    console.log('afterChange');
-  }
-  
-  beforeChange(e:any) {
-    console.log('beforeChange');
-  }
 
 }
